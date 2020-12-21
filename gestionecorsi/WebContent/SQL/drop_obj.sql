1b945bba6b85a30b246846d3d2e63606fca9def1
Gianluca Celante <gianlucacelante@gmail.com>
2020-12-21 17:19:27 +0100

    creazione tabelle db

---
+++ gestionecorsi/WebContent/SQL/drop_obj.sql
@@ -0,0 +1,13 @@
+drop table corso cascade constraints;
+
+drop table corsista cascade constraints;
+
+drop table corso_corsista cascade constraints;
+
+drop table docente cascade constraints;
+
+drop table amministratore cascade constraints;
+
+drop sequence corso_seq;
+
+drop sequence corsista_seq;
