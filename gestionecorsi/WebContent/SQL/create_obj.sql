a806a89285746d741aa439fdaf2b160bf79bb83b
Jonathan <Betacom@risorsa3.station>
2020-12-21 17:26:11 +0100

    Riordino tabelle

--- gestionecorsi/WebContent/SQL/create_obj.sql
+++ gestionecorsi/WebContent/SQL/create_obj.sql
@@ -1,14 +1,28 @@
 create table amministratore(
-
 nomeadmin varchar2(30),
 cognomeadmin varchar2(30),
 codadmin int,
 constraint p_codadmin primary key(codadmin)
+)
 
+create table corsista(
+nomecorsista varchar2(30),
+cognomecorsista varchar2(30),
+codcorsista int,
+precedentiformativi number(1),
+constraint p_codcorsista primary key(codcorsista),
+constraint check_bool check (precedentiformativi=1 or precedentiformativi=0)
+)
+
+create table docente(
+nomedocente varchar2(30),
+cognomedocente varchar2(30),
+cvdocente varchar2(30),
+coddocente int,
+constraint p_coddocente primary key(coddocente)
 )
 
 create table corso(
-
 codcorso int,
 coddocente int,
 nomecorso varchar2(30),
@@ -19,41 +33,14 @@ commenticorso varchar2(200),
 aulacorso varchar2(30),
 constraint p_codcorso primary key(codcorso),
 constraint f_coddocente foreign key(coddocente) references docente(coddocente) on delete cascade
-
-
-)
-
-create table corsista(
-
-nomecorsista varchar2(30),
-cognomecorsista varchar2(30),
-codcorsista int,
-precedentiformativi number(1),
-constraint p_codcorsista primary key(codcorsista),
-constraint check_bool check (precedentiformativi=1 or precedentiformativi=0)
-
 )
 
 create table corso_corsista(
-
 codcorso int,
 codcorsista int,
 constraint f_codcorso foreign key(codcorso) references corso(codcorso) on delete cascade,
 constraint f_codcorsista foreign key(codcorsista) references corsista(codcorsista) on delete cascade,
 constraint p_cc primary key(codcorso, codcorsista)
-
-)
-
-
-
-create table docente(
-
-nomedocente varchar2(30),
-cognomedocente varchar2(30),
-cvdocente varchar2(30),
-coddocente int,
-constraint p_coddocente primary key(coddocente)
-
 )
 
 create sequence corso_seq;
