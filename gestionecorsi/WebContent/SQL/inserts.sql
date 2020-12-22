f6c6d80609989e34f7d1c2f0f35792a3726ceff7
Gianluca Celante <gianlucacelante@gmail.com>
2020-12-22 13:45:36 +0100

    correzione inserts

--- gestionecorsi/WebContent/SQL/inserts.sql
+++ gestionecorsi/WebContent/SQL/inserts.sql
@@ -1,7 +1,7 @@
-insert into docente values('Massimo', 'Rossi', 'cv', 1)
-insert into docente values('Paolo', 'Verdi', 'cv', 2)
-insert into docente values('Luigi', 'Bianchi', 'cv', 3)
-insert into docente values('Laura', 'Neri', 'cv', 4)
+insert into docente values('Massimo', 'Rossi', 'cv', 1);
+insert into docente values('Paolo', 'Verdi', 'cv', 2);
+insert into docente values('Luigi', 'Bianchi', 'cv', 3);
+insert into docente values('Laura', 'Neri', 'cv', 4);
 
-insert into amministratore values('admin', 'admimn', 1)
+insert into amministratore values('admin', 'admimn', 1);
 
