9a899260ec4ef1424b186bb1ac0bbf46788eb290
Gianluca Celante <gianlucacelante@gmail.com>
2020-12-22 14:15:47 +0100

    modify inserts sql

--- gestionecorsi/WebContent/SQL/inserts.sql
+++ gestionecorsi/WebContent/SQL/inserts.sql
@@ -3,5 +3,5 @@ insert into docente values('Paolo', 'Verdi', 'cv', 2);
 insert into docente values('Luigi', 'Bianchi', 'cv', 3);
 insert into docente values('Laura', 'Neri', 'cv', 4);
 
-insert into amministratore values('admin', 'admin', 1);
+insert into amministratore values('admin', 'admin', 1, 'pass');
 
