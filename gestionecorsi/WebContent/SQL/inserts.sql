ef5959249720d89050ba9b7415c9d9cb11496f26
betacom <betacom@Betacom-PC7M5H>
2020-12-23 12:39:00 +0100

    inserito insert dei corsi

--- gestionecorsi/WebContent/SQL/inserts.sql
+++ gestionecorsi/WebContent/SQL/inserts.sql
@@ -5,3 +5,5 @@ insert into docente values('Laura', 'Neri', 'cv', 4);
 
 insert into amministratore values('admin', 'admin', 1, 'pass');
 
+insert into corso values(1,1,'Informatica','20-MAG-2020','24-MAG-2020',50,'informatica','A5',15);
+insert into corso values(2,4,'Java','20-GIU-2020','24-LUG-2020',50,'informatica','A1',10);
